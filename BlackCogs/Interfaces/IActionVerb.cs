66dee024c6e5d0ab31538c7045c2900755ec70c0
angaratosurion <angarato.surion@gmail.com>
2015-12-17 22:59:43 +0200

    initial
    aaa

---
+++ BlackCogs/Interfaces/IActionVerb.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackCogs.Interfaces
+{
+    public interface IActionVerb
+    {
+        #region Properties
+        /// <summary>
+        /// Gets the name of the verb.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Gets the action.
+        /// </summary>
+        string Action { get; }
+
+        /// <summary>
+        /// Gets the controller.
+        /// </summary>
+        string Controller { get; }
+        string Description { get; }
+        #endregion
+    }
+}
