c242b068238053ca05d8605ae2f9ec118b42c57a
angaratosurion <angarato.surion@gmail.com>
2015-12-18 02:13:26 +0200

    aa
    aaa

---
+++ BlackCogs/ModuleInfo/Info.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BlackCogs.Interfaces;
+
+namespace BlackCogs.ModuleInfo
+{
+    [Export(typeof(IModuleInfo))]
+    public class Info : IModuleInfo
+    {
+        public string Description
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "BlackCogs";
+            }
+        }
+
+        public string SourceCode
+        {
+            get
+            {
+                return "https://github.com/angaratosurion/BlackCogs";
+            }
+        }
+
+        public string Version
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public string WebSite
+        {
+            get
+            {
+                return "http://pariskoutsioukis.net/blog/";
+            }
+        }
+    }
+}
