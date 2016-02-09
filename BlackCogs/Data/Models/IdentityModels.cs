29d13bda9e79cac5624d077385003d9fb3045c8c
angaratosurion <angarato.surion@gmail.com>
2016-02-09 23:24:53 +0200

    aa
    aa

--- BlackCogs/Data/Models/IdentityModels.cs
+++ BlackCogs/Data/Models/IdentityModels.cs
@@ -13,14 +13,59 @@ namespace BlackCogs.Data.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public ApplicationUser Clone()
+        {
+            try
+            {
+                ApplicationUser ap = new ApplicationUser();
+                
+                //if ( this !=null)
+                {
+                    ap.Id = this.Id;
+                    ap.UserName = this.UserName;
+                   ap.AccessFailedCount = this.AccessFailedCount;
+                    ap.LockoutEnabled = this.LockoutEnabled;
+                    ap.LockoutEndDateUtc = this.LockoutEndDateUtc;
+                    ap.PasswordHash = this.PasswordHash;
+                    ap.PhoneNumber = this.PhoneNumber;
+                    ap.PhoneNumberConfirmed = this.PhoneNumberConfirmed;
+                    ap.SecurityStamp = this.SecurityStamp;
+                    ap.TwoFactorEnabled = this.TwoFactorEnabled;
+                    ap.Email = this.Email;
+                    ap.EmailConfirmed = this.EmailConfirmed;
+                    var roles = this.Roles;
+                     foreach( var rol in roles)
+                    {
+                        ap.Roles.Add(rol);
+                    }
+                    var claims = this.Claims;
+                    foreach(var claim in claims)
+                    {
+                        ap.Claims.Add(claim);
+                    }
+                    var logins = this.Logins;
+                    foreach(var login in logins)
+                    {
+                        ap.Logins.Add(login);
+                    }
 
+                }
+                return ap;
+
+            }
+            catch (Exception ex)
+            {
+                CommonTools.ErrorReporting(ex); 
+                return null;
+            }
+        }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
+            var thisIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom this claims here
 
-            return userIdentity;
+            return thisIdentity;
         }
         
     }
