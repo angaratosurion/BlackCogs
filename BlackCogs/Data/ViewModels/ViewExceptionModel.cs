db5aec448a949c1e275f3c1c1a1c11356dc86dfd
angaratosurion <angarato.surion@gmail.com>
2016-12-19 13:04:31 +0200

    aaa
    adding viewmodel for catched exceptions

---
+++ BlackCogs/Data/ViewModels/ViewExceptionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackCogs.Data.ViewModels
+{
+    public class ViewExceptionModel
+    {
+        public DateTime DTime { get; set; }
+        public string   Level { get; set; }
+        public string Message { get; set; }
+    }
+}
