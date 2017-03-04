c4bc26fe1f9c41477189bc3ac49c1dde9a07accd
angaratosurion <angarato.surion@gmail.com>
2017-03-04 03:39:43 +0200

    dd
    dd

---
+++ BlackCogs/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BlackCogs.Controllers
+{
+    [Export("Home", typeof(IController))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
